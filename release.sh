01e6cf0a707d96ea1ff6acdb1133ab2425e011b2
kinshuk.bairagi <me@kinshuk.in>
2020-01-08 14:59:29 +0530

    deb8 cmake support + fix deb create script

--- release.sh
+++ release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+VERSION=$(date +%s)
 fpm --force \
   --input-type dir --output-type deb\
-  --version "0.2" --name tcping\
+  --version "0.2.$VERSION" --name tcping\
   --architecture amd64\
   --prefix /\
   --description 'tcping'\
   --url "https://github.com/kingster/tcping"\
+  --maintainer "me@kinshuk.in" \
   --no-deb-systemd-restart-after-upgrade \
   --package tcping.deb \
-  ./tcping=/usr/bin
+  ./tcping=/usr/sbin/tcping
 
